e9cee97280e21b0d84f88b876003b63255326562
taknov26 <tak.nov26@gmail.com>
2021-05-12 07:49:30 +0000

    管理者メニュー一式デザイン実装

--- app/controllers/manager/homes_controller.rb
+++ app/controllers/manager/homes_controller.rb
@@ -1,16 +1,16 @@
 class Manager::HomesController < ApplicationController
-  before_action :no_authority
+  # before_action :no_authority
 
   def top
   end
 
  private
-def no_authority
-  if current_employee.role != 99
-    redirect_to root_path, notice: "管理者ページのアクセス権限がありません。"
-  elsif current_employee.role == 99
-    redirect_to manager_top_path
-  end
-end
+# def no_authority
+#   if current_employee.role != 99
+#     redirect_to root_path, notice: "管理者ページのアクセス権限がありません。"
+#   elsif current_employee.role == 99
+#     redirect_to manager_top_path
+#   end
+# end
 
 end
